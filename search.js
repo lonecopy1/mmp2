8317d045e240864ca1c1f1832761ffcd0612f305
IffyComet <47402722+IffyComet@users.noreply.github.com>
2019-05-23 12:06:41 +0200

    søgefunktion

--- search.js
+++ search.js
@@ -1,11 +1,10 @@
-var installationer = ["Kloak Syd", "Kloak Nord", "Kloak Øst", "Kloak Vest", "Lygtepæle", "Hestestald", "Kirkegård", "Mad transport"];
+/*var installationer = ["Kloak Syd", "Kloak Nord", "Kloak Øst", "Kloak Vest", "Lygtepæle", "Hestestald", "Kirkegård", "Mad transport"];
 var instalLen = installationer.length;
 
 function installationsListe() {
     var input, filter, i;
     input = document.getElementById("searchInput").value;
     filter = input.toUpperCase();
-    console.log(filter);
     
     for (i = 0; i < instalLen; i++) {
         
@@ -13,5 +12,29 @@ function installationsListe() {
         
         
     }
-    /* document.getElementById("searchResult").innerHTML = */
+    /* document.getElementById("searchResult").innerHTML = 
+}*/
+
+
+function installationsListe() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("searchInput").value;
+    filter = input.toUpperCase();
+    ul = document.getElementById("searchResult");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("p")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            document.getElementById("searchResult").style.visibility = "visible";
+            document.getElementsByClassName("tekstboks").style.visibility = "visible";
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+function testTest() {
+    for ()
 }
